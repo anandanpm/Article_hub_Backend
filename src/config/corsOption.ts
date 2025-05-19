1a3eff1ddb9bbaed90e2b947d9e0d25b6788f9d8
anandanpm <ananda1732001@gmail.com>
2025-05-19 14:25:48 +0530

    the things are done

--- src/config/corsOption.ts
+++ src/config/corsOption.ts
@@ -1,9 +1,24 @@
+// import { CorsOptions } from 'cors';
+
+// export const corsOptions: CorsOptions = {
+//   origin: [process.env.FRONTEND_URL].filter(
+//     (origin): origin is string => typeof origin === 'string'
+//   ),
+//   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+//   credentials: true,
+// };
+
+
 import { CorsOptions } from 'cors';
 
+const allowedOrigin = process.env.FRONTEND_URL;
+
+if (!allowedOrigin) {
+  console.warn('⚠️ FRONTEND_URL is not defined in environment variables');
+}
+
 export const corsOptions: CorsOptions = {
-  origin: [process.env.FRONTEND_URL].filter(
-    (origin): origin is string => typeof origin === 'string'
-  ),
+  origin: allowedOrigin,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
 };
--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,12 @@ import cors from 'cors';
 import { connectDB } from './services/db';
 import { corsOptions } from "./config/corsOption";
 import userRoute from './routes/userRoute';
-
+dotenv.config();
 const app = express();
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
-dotenv.config();
+
 
 app.use((req, res, next) => {
     console.log(`Incoming Request: ${req.method} ${req.url}`);
